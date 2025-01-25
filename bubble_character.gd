e3974b9e7e23ee9044f18dd2140dad0beb48dcab
Zshandi Krahn <ZshandiK@gmail.com>
2025-01-25 11:22:05 -0800

    Added lots of stuff
    All the stuff was added
    
    Now have bubble that moves and pops and stuff

--- bubble_character.gd
+++ bubble_character.gd
@@ -1,33 +1,107 @@
 extends RigidBody2D
 class_name BubbleCharacter
 
+# Normally this would be 2
+@export
+var area_pow := 2.0
+
+@export
+var hue_scale := 1.0/345.67
+
+@export
+var bubble_speed := 100.0
+
+@onready
+var starting_scale:Vector2 = %BubbleCollision.scale
+
+signal started
+signal died
+
+var is_dead := false
+var is_starting := false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	get_tree().paused = true
+	process_mode = PROCESS_MODE_ALWAYS
+	is_starting = true
+	
+	print_debug("Starting scale: ", %BubbleCollision.global_scale)
+	print_debug("Starting hue: ", %BubbleColor.modulate.h)
+	
+	assert(is_equal_approx(0.5, size_to_area(area_to_size(0.5))))
+	assert(is_equal_approx(0.5, area_to_size(size_to_area(0.5))))
+	assert(is_equal_approx(1, size_to_area(area_to_size(1))))
+	assert(is_equal_approx(1, area_to_size(size_to_area(1))))
+	assert(is_equal_approx(3, size_to_area(area_to_size(3))))
+	assert(is_equal_approx(3, area_to_size(size_to_area(3))))
+
+func get_normalized_direction() -> Vector2:
+	var direction = -(get_global_mouse_position() - global_position)
+	return direction.normalized()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("click"):
-		var direction = get_global_mouse_position() - global_position
-		direction = direction.normalized()
+	if Input.is_action_just_pressed("move"):
+		if is_starting:
+			$AnimationPlayer.play("wobble")
+			process_mode = PROCESS_MODE_INHERIT
+			get_tree().paused = false
+			is_starting = false
+			started.emit()
 		
-		apply_central_impulse(direction * 200)
+		var direction = get_normalized_direction()
+		
+		apply_central_impulse(direction * bubble_speed)
+	
+	if get_colliding_bodies().size() > 0:
+		die()
+	
+	# Debug
+	if OS.is_debug_build():
+		if Input.is_action_just_pressed("spawn_bubble"):
+			# Spawn a new bubble at cursor
+			var bubble = preload("res://bubble_collectible.tscn").instantiate()
+			get_tree().root.add_child(bubble)
+			bubble.owner = get_tree().root
+			bubble.global_position = get_global_mouse_position()
+		
+		if Input.is_action_just_pressed("reset_size"):
+			%BubbleCollision.scale = starting_scale
+
+
+func die():
+	is_dead = true
+	died.emit()
+	get_tree().paused = true
+	await get_tree().create_timer(0.25).timeout
+	$AnimationPlayer.process_mode = PROCESS_MODE_ALWAYS
+	$AnimationPlayer.play("pop")
+	await $AnimationPlayer.animation_finished
+	await get_tree().create_timer(1).timeout
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+
+func size_to_area(radius:float) -> float:
+	return PI*pow(radius, area_pow)
+
+func area_to_size(area:float) -> float:
+	return pow(area/PI, 1.0/area_pow)
 
 func collect_bubble(size: float):
-	var current_scale = $BubbleCollision.scale.x
+	var current_scale = %BubbleCollision.global_scale.x
+	var current_size = %BubbleCollision.shape.radius * current_scale
 	
-	var current_size = $BubbleCollision.shape.radius * current_scale
+	var current_area = size_to_area(current_size)
+	var added_area = size_to_area(size)
+	var new_area = current_area + added_area
+	var new_size = area_to_size(new_area)
 	
-	#var current_volume = PI*pow(current_size, 3)*4.0/3.0
-	#var added_volume = PI*pow(size, 3)*4.0/3.0
-	#var new_volume = current_volume + added_volume
-	#
-	#var new_size = pow(new_volume, 1.0/3.0)/PI/(4.0/3.0)
+	print_debug("new_size: ", new_size, ", current_size: ", current_size)
 	
-	var current_volume = PI*pow(current_size, 2)
-	var added_volume = PI*pow(size, 2)
-	var new_volume = current_volume + added_volume
+	%BubbleCollision.global_scale *= new_size / current_size
+	print_debug("New scale: ", %BubbleCollision.global_scale)
 	
-	var new_size = pow(new_volume, 1.0/2.0)/PI
-	
-	$BubbleCollision.scale *= new_size / current_size
+	# Change the color as we grow
+	%BubbleColor.modulate.h = fmod(%BubbleColor.modulate.h + size * hue_scale, 1.0)
+	print_debug("New hue: ", %BubbleColor.modulate.h)
--- bubble_collectible.gd
+++ bubble_collectible.gd
@@ -3,7 +3,7 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	%BubbleColor.modulate.h = randf()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,7 +13,10 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is BubbleCharacter:
-		var char = body as BubbleCharacter
-		# TODO
-		char.collect_bubble(50)
+		var character = body as BubbleCharacter
+		
+		var current_scale = %BubbleCollision.global_scale.x
+		var current_size = %BubbleCollision.shape.radius * current_scale
+		
+		character.collect_bubble(current_size)
 		queue_free()
