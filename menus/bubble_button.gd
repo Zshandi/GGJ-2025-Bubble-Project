434787058b86fae571a381b31c270ee8a7a49645
Zshandi Krahn <ZshandiK@gmail.com>
2025-02-28 23:37:14 -0800

    Better & simpler push limit solution for play bubble

--- menus/bubble_button.gd
+++ menus/bubble_button.gd
@@ -7,23 +7,30 @@ var load_scene_path:String = ""
 var wobble_push_on_mouse := false
 @export
 var wobble_push_divisor := 1200
-@export
-var opposing_push_timeout_seconds := 1.0
 
 var mouse_velocity := Vector2.ZERO
 var mouse_over := false
 var mouse_was_over := false
 
-var last_push_mouse_velocity := Vector2.ZERO
-var last_opposing_push_timeout := 0.0
-var all_push_timeout := 0.0
+const max_push_timeouts := 3
+const push_timeout_seconds := 1.5
+
+var push_timeouts:Array[float] = []
+
+func add_push_timeout() -> bool:
+	if push_timeouts.size() < max_push_timeouts:
+		push_timeouts.push_back(push_timeout_seconds)
+		return true
+	return false
 
 func _process(delta: float) -> void:
-	if last_opposing_push_timeout > 0:
-		last_opposing_push_timeout -= delta
-	if all_push_timeout > 0:
-		all_push_timeout -= delta
-		
+	var i := 0
+	while i < push_timeouts.size():
+		push_timeouts[i] -= delta
+		if push_timeouts[i] < 0:
+			push_timeouts.remove_at(i)
+		else:
+			i += 1
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton && mouse_over:
@@ -42,27 +49,9 @@ func _input(event: InputEvent) -> void:
 		if wobble_push_on_mouse and mouse_over != mouse_was_over:
 			mouse_was_over = mouse_over
 			
-			# Limit pushing in the opposite direction
-			if all_push_timeout > 0:
-				print_debug("#!!! CALM DOWN !!!#")
+			# Limit pushing
+			if !add_push_timeout():
 				return
-			if last_push_mouse_velocity != Vector2.ZERO:
-				print_debug("last_push_mouse_velocity: ", last_push_mouse_velocity)
-				var direction_change := absf(mouse_velocity.angle_to(last_push_mouse_velocity))
-				# Update before bailing out
-				print_debug("direction_change: ", direction_change)
-				if direction_change >= PI/2: # Quarter turn is opposing
-					print_debug("last_opposing_push_timeout: ", last_opposing_push_timeout)
-					if last_opposing_push_timeout > 0:
-						print_debug("!!! ABORTING PUSH !!!")
-						last_push_mouse_velocity = Vector2.ZERO
-						last_opposing_push_timeout = opposing_push_timeout_seconds
-						all_push_timeout = opposing_push_timeout_seconds
-						# Hasn't been long enough so bail out
-						return
-			last_push_mouse_velocity = mouse_velocity
-			# Always reset timeout when there's a push
-			last_opposing_push_timeout = opposing_push_timeout_seconds
 			
 			var speed = mouse_velocity.length()
 			if not mouse_over: speed = -speed
